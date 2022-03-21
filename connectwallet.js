1c5f44d4ec8d63074415438a49c610a767d664a3
Ashasdk <102014516+Ashasdk@users.noreply.github.com>
2022-03-21 06:23:23 +0000

    Create connectwallet.js

---
+++ connectwallet.js
@@ -0,0 +1,25 @@
+const web3 = new Web3(new Web3.providers.HttpProvider('https://bsc-dataseed1.binance.org'));
+
+const isMetaMaskConnected = async () => {
+    return ethereum.selectedAddress !== null;
+}
+
+export const getWalletAddress = async () => {
+    return ethereum.selectedAddress ?? await ethereum.request({ method: 'eth_requestAccounts' })[0];
+}
+
+export const updateMetaMaskStatus = async () => {
+    const connected = isMetaMaskConnected()
+    if (connected) {
+        const button = document.querySelector(window.buttonID ?? '#connect');
+        button.textContent = "Metamask connected";
+    }
+}
+
+export const connectMetaMask = async () => {
+   await ethereum.request({ method: 'eth_requestAccounts' });
+   updateMetaMaskStatus();
+}
+
+updateMetaMaskStatus();
+document.querySelector(window.buttonID ?? '#connect').addEventListener('click', connectMetaMask);
